56cae444f01be625b9f83ad933f524376671fe42
Veddersboy <Veddersboy2005@gmail.com>
2025-07-03 20:59:24 +0900

    slight code refinements using 'enum'

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,10 @@ var crouch_velocity = 0
 
 var maxSpeed = 100 * 1.5
 var lastDirection := Vector2(1, 0)
+
+enum player_state{ IDLE, RUN, JUMP, FALL, CROUCH}
+var state = player_state.IDLE
+
 var playerState = "idle"
 
 func _physics_process(delta):
@@ -23,40 +27,53 @@ func _physics_process(delta):
 			velocity.x = direction.x * maxSpeed
 	else:
 		velocity.x = direction.x * maxSpeed
+	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	elif Input.is_action_just_pressed("jump"):
 		velocity.y = jump_velocity
 	
+	# Small jump
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 			velocity.y *= jump_cut_multiplier
 	
 	move_and_slide()
 	
-	if direction == Vector2.ZERO:
-		playerState = "idle"
+	if direction.x != 0:
+		lastDirection.x = direction.x
+	
+	# Jump, Crouch, and run physics.
+	if not is_on_floor():
+		if velocity.y < 0:
+			state = player_state.JUMP
+		else:
+			state = player_state.FALL
+	elif Input.is_action_pressed("crouch"):
+		state = player_state.CROUCH
+	elif direction != Vector2.ZERO:
+		state = player_state.RUN
 	else:
-		playerState = "walking"
-		lastDirection = direction
+		state = player_state.IDLE
 	
 	play_walk_animation(direction, direction_up_and_down)
 
 func play_walk_animation(direction: Vector2, vertical_input: Vector2):
-	var flip = direction.x < 0 if direction.x != 0 else lastDirection.x < 0
-	$AnimatedSprite2D.flip_h = flip
+	$AnimatedSprite2D.flip_h = lastDirection.x < 0
 	
 	if Input.is_action_just_pressed("attack"):
 		$AnimatedSprite2D.play("base_attack")
+		return
 	
-	if not is_on_floor():
-		if velocity.y < 0:
+	match state:
+		player_state.IDLE:
+			$AnimatedSprite2D.play("idle_right")
+		player_state.RUN:
+			$AnimatedSprite2D.play("move_right")
+		player_state.JUMP:
 			$AnimatedSprite2D.play("jump")
-		else:
+		player_state.FALL:
 			$AnimatedSprite2D.play("fall")
-	elif Input.is_action_pressed("crouch"):
-		$AnimatedSprite2D.play("crouch")
-	elif direction == Vector2.ZERO:
-		$AnimatedSprite2D.play("idle_right")
-	else:
-		$AnimatedSprite2D.play("move_right")
-	
+		player_state.CROUCH:
+			$AnimatedSprite2D.play("crouch")
+		_:
+			$AnimatedSprite2D.play("idle_right") # Default
