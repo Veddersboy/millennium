3a6fa912f086976b3b23e2b9f770542af0250c18
CorruptDTA <124949648+CorruptDTA@users.noreply.github.com>
2025-07-09 20:51:17 -0400

    Fixed transition from fall state into idle

--- scripts/fall.gd
+++ scripts/fall.gd
@@ -3,16 +3,25 @@ class_name Fall
 
 @export
 var idle_state: State
+@export
+var move_state: State
+@export
+var minSpeed : float = 0.005
 
 func enter():
 	super()
 	parent.animations.play("fall")
 
 func process_physics(delta) -> State:
+	var input_dir = Input.get_axis("move_left", "move_right")
+	parent.velocity.x = move_toward(parent.velocity.x, input_dir * parent.maxSpeed, parent.acceleration * delta)
 	parent.velocity.y += parent.GRAVITY * delta
 
 	if parent.is_on_floor():
-		return idle_state
+		if input_dir != 0 && abs(parent.velocity.x) < minSpeed:
+			return idle_state
+		else:
+			return move_state
 
 	parent.move_and_slide()
 	return null
